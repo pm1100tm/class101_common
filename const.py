9d47791ec49ea36fc90f79152ec5b2015145781e
dev-swd <tladnjsen@gmail.com>
2022-02-06 02:20:05 +0900

    <common> feature/common-utils: set common const and util - add const class - modify CommonUtil method print_log, add return_data

---
+++ const.py
@@ -0,0 +1,33 @@
+class CommonConst:
+    BLANK_STR = ''
+
+
+class AppNameConst:
+    MENU    = 'menu'
+    ACCOUNT = 'account'
+    PRODUCT = 'product'
+
+
+class MethodNameConst:
+    CREATE   = 'create'
+    UPDATE   = 'create'
+    LIST     = 'create'
+    RETRIEVE = 'retrieve'
+    GET      = 'get'
+    POST     = 'post'
+    DELETE   = 'delete'
+    
+    SIGN_UP_KAKAO             = 'social_signup'
+    SIGN_UP_KAKAO_CALLBACK    = 'signup_kakao_callback'
+    SIGN_UP_KAKAO_GET_PROFILE = 'signup_kakao_get_profile'
+    
+
+class ResponseMsgConst:
+    SUCCESS    = 'SUCCESS'
+    FAIL       = 'FAIL'
+    NO_CONTENT = 'NO_CONTENT'
+    
+    KEY_ERROR             = 'KEY_ERROR'
+    ATTRIBUTE_ERROR       = 'ATTRIBUTE_ERROR'
+    VALUE_ERROR           = 'VALUE_ERROR'
+    ATTRIBUTE_VALUE_ERROR = 'ATTRIBUTE/VALUE_ERROR'
