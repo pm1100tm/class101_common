6367698afbe7f73ba438f2d8f008a6909b0fc31a
dev-swd <tladnjsen@gmail.com>
2022-03-04 10:32:30 +0900

    <feature/authentication>: 보인 인증 모듈 수정 및 공통 유틸 모듈화 - 본인 인증 모듈 변수명 변경 - 공통유틸과 시간 관련 유틸 모듈 분리

--- self_authentication.py
+++ self_authentication.py
@@ -11,99 +11,105 @@ from Crypto.Cipher                          import AES
 from hmac                                   import HMAC
 
 from django.conf                            import settings
-from common.exceptions                      import NiceAPIException, DataEncryptoException
+
+from common.const                           import CommonConst
+from common.util_date                       import TimeUtils
+from common.exceptions                      import SelfAuthException, DataEncryptoException
 
 
-class NiceAPI:
-    NICE_API_HOST               = settings.NICE_API_HOST
-    NICE_URI_ISSUE_TOKEN        = settings.NICE_URI_ISSUE_TOKEN
-    NICE_URI_ISSUE_CRYPTO_TOKEN = settings.NICE_URI_ISSUE_CRYPTO_TOKEN
-    NICE_URI_REVOKE_TOKEN       = settings.NICE_URI_REVOKE_TOKEN
-    NICE_CLIENT_ID              = settings.NICE_CLIENT_ID
-    NICE_CLIENT_SECRET          = settings.NICE_CLIENT_SECRET
-    NICE_PRODUCT_ID             = settings.NICE_PRODUCT_ID
-    CONTENT_TYPE_X_WWW          = 'application/x-www-form-urlencoded;charset=utf-8'
-    CONTENT_TYPE_AP_JSON        = 'application/json'
+class N_API:
+    N_API_HOST               = settings.N_API_HOST
+    N_URI_ISSUE_TOKEN        = settings.N_URI_ISSUE_TOKEN
+    N_URI_ISSUE_CRYPTO_TOKEN = settings.N_URI_ISSUE_CRYPTO_TOKEN
+    N_URI_REVOKE_TOKEN       = settings.N_URI_REVOKE_TOKEN
+    N_CLIENT_ID              = settings.N_CLIENT_ID
+    N_CLIENT_SECRET          = settings.N_CLIENT_SECRET
+    N_PRODUCT_ID             = settings.N_PRODUCT_ID
+    CONTENT_TYPE_X_WWW       = CommonConst.CONTENT_TYPE_X_WWW
+    CONTENT_TYPE_AP_JSON     = CommonConst.CONTENT_TYPE_APP_JSON
     
     def issue_institution_token(self) -> dict:
-        """ NICE API 기관용 토큰 발급
+        """ N*** API - 기관용 토큰 발급
+            *param
+                - None
+            
             *return
-            - access_token_data (dict: 발급받은 기관용 토큰, 만료 시간)
+                - access_token_data (dict: 발급받은 기관용 토큰, 만료 시간)
         """
-        url = self.NICE_API_HOST + self.NICE_URI_ISSUE_TOKEN
-        token_auth_key = base64.b64encode(f'{self.NICE_CLIENT_ID}:{self.NICE_CLIENT_SECRET}'.encode()).decode()
-    
+        url = self.N_API_HOST + self.N_URI_ISSUE_TOKEN
+        token_auth_key = base64.b64encode(f'{self.N_CLIENT_ID}:{self.N_CLIENT_SECRET}'.encode()).decode()
+        
         headers = {
             'Content-Type' : self.CONTENT_TYPE_X_WWW,
             'Authorization': f'Basic {token_auth_key}',
         }
-    
+        
         req_data = {
             'scope'     : 'default',
             'grant_type': 'client_credentials'
         }
-    
+        
         response = requests.post(url=url,
                                  data=req_data,
                                  headers=headers).json()
-    
+        
         data_header = response['dataHeader']
         data_body = response['dataBody']
-    
+        
         if data_header['GW_RSLT_CD'] != '1200':
-            raise NiceAPIException(data_header['GW_RSLT_MSG'])
-    
+            raise SelfAuthException(data_header['GW_RSLT_MSG'])
+        
         access_token_data = {
             'access_token': data_body['access_token'],
             'expires_in'  : data_body['expires_in'],
         }
-    
+        
         return access_token_data
-
+    
     def discard_institution_token(self, access_token: str) -> None:
-        """ NICE API 기관용 토큰 폐기
+        """ N API 기관용 토큰 폐기
             param
             - access_token (만료할 기관용 토큰)
 
             return
-            - tuple(bool, str) (요청 성공시 True, NiceAPI 요청 실패 시 False)
+            - tuple(bool, str) (요청 성공시 True, N_API 요청 실패 시 False)
         """
-        url = self.NICE_API_HOST + self.NICE_URI_REVOKE_TOKEN
+        url = self.N_API_HOST + self.N_URI_REVOKE_TOKEN
         
-        current_timestamp = str(int(get_time.time()))
-        token_auth_key = base64.b64encode(f'{access_token}:{current_timestamp}:{self.NICE_CLIENT_ID}'.encode()).decode()
-    
+        current_timestamp = TimeUtils.get_time_seconds()
+        token_auth_key    = base64.b64encode(f'{access_token}:{current_timestamp}:{self.N_CLIENT_ID}'.encode()).decode()
+        
         headers = {
             'Content-Type' : self.CONTENT_TYPE_X_WWW,
             'Authorization': f'Basic {token_auth_key}',
         }
-    
+        
         response = requests.post(url=url, headers=headers).json()
         data_header = response['dataHeader']
         
         if data_header['GW_RSLT_CD'] != '1200':
-            raise NiceAPIException(data_header['GW_RSLT_MSG'])
+            raise SelfAuthException(data_header['GW_RSLT_MSG'])
 
     def issue_crypto_token(self, access_token):
-        """ NICE API 암호화 토큰 발행
+        """ N API 암호화 토큰 발행
             *param
             - access_token (만료할 기관용 토큰)
 
             *return
-            - crypto_token_data (NICE 표준창 서비스 요청에 필요한 data set)
+            - crypto_token_data (N 표준창 서비스 요청에 필요한 data set)
         """
-        url = self.NICE_API_HOST + self.NICE_URI_ISSUE_CRYPTO_TOKEN
+        url = self.N_API_HOST + self.N_URI_ISSUE_CRYPTO_TOKEN
         
-        current_timestamp = str(int(get_time.time()))
-        crypto_token_auth_key = base64.b64encode(f'{access_token}:{current_timestamp}:{self.NICE_CLIENT_ID}'.encode()).decode()
+        current_timestamp     = TimeUtils.get_time_seconds()
+        crypto_token_auth_key = base64.b64encode(f'{access_token}:{current_timestamp}:{self.N_CLIENT_ID}'.encode()).decode()
         
-        req_dtim = datetime.today().strftime('%Y%m%d%H%M%S')
-        req_no = f"{'Req'}{datetime.today().strftime('%Y%m%d%H%M%S%f')}"
+        req_dtim = TimeUtils.get_today_ymdhms()
+        req_no   = f"{'Req'}{TimeUtils.get_today_ymdhmsf()}"
         
         headers = {
             'Content-Type' : self.CONTENT_TYPE_AP_JSON,
             'Authorization': f'bearer {crypto_token_auth_key}',
-            'productID'    : self.NICE_PRODUCT_ID,
+            'productID'    : self.N_PRODUCT_ID,
         }
         
         req_data = {
@@ -122,7 +128,7 @@ class NiceAPI:
                                  headers=headers).json()
         
         data_header = response['dataHeader']
-        data_body = response['dataBody']
+        data_body   = response['dataBody']
         
         if data_header['GW_RSLT_CD'] != '1200':
             
@@ -131,7 +137,7 @@ class NiceAPI:
                 test = str(data_body['rsp_cd'])
                 
                 msg += data_body['res_msg'] if test.startswith('EAPI') else ''
-                raise NiceAPIException(msg)
+                raise SelfAuthException(msg)
         
         crypto_token_data = {
             # request_data
@@ -147,7 +153,7 @@ class NiceAPI:
         }
         
         return crypto_token_data
-
+    
     def generate_sha_signature(self, crypto_token_data) -> bytes:
         """ 데이터 암호화 대칭키(key, iv), 무결성키 생성을 위한 Sha256 해싱 값
             req_dtim.trim() + req_no.trim() + token_val.trim() 후 Sha256 및 base64 encoding
@@ -156,25 +162,25 @@ class NiceAPI:
         sha_signature: bytes = hashlib.sha256(material_to_encrypt.encode()).digest()
         base64_code: bytes = base64.b64encode(sha_signature)
         return base64_code
-
+    
     def generate_key(self, base64_code: bytes) -> bytes:
         """ 데이터 암호화 대칭키 (key)
         """
         key: bytes = base64_code[:16]
         return key
-
+    
     def generate_iv(self, base64_code: bytes) -> bytes:
         """ 데이터 암호화 Initial Vector (iv)
         """
         iv: bytes = base64_code[-16:]
         return iv
-
+    
     def generate_hmac_key(self, base64_code: bytes) -> bytes:
         """ 무결성키 생성에 필요한 키 (암호화값 위변조 체크용)
         """
         hmac_key: bytes = base64_code[:32]
         return hmac_key
-
+    
     def generate_integrity_value(self, encrypto_data, hmac_key) -> str:
         """ 무결성키 생성
         """
@@ -182,7 +188,7 @@ class NiceAPI:
         hm = HMAC(hmac_key, enc_data, hashlib.sha256)
         hmac_auth = base64.b64encode(hm.digest()).decode()
         return hmac_auth
-
+    
     def generate_kihmac_data(self, crypto_token_data):
         """ 위의 key, iv, hmac 키 데이터 세트 리턴
         """
@@ -192,7 +198,7 @@ class NiceAPI:
             'iv_value' : self.generate_iv(base64_code=base64_code),
             'hmac_key' : self.generate_hmac_key(base64_code=base64_code),
         }
-    
+        
         return kihmac_data
 
 
@@ -201,7 +207,7 @@ class AESCipherCBC:
     """
     BS         = algorithms.AES.block_size
     AES_MODE   = AES.MODE_CBC
-    RETURN_URL = settings.NICE_RETURN_URL
+    RETURN_URL = settings.N_RETURN_URL
     
     def pkcs7_padding(self, data) -> bytes:
         """ PKCS7 패딩
@@ -212,7 +218,7 @@ class AESCipherCBC:
         try:
             padder = padding.PKCS7(self.BS).padder()
             padded_data = padder.update(data) + padder.finalize()
-            
+        
         except ValueError:
             traceback.print_exc()
             raise DataEncryptoException('pkcs7_padding error!')
@@ -264,7 +270,7 @@ class AESCipherCBC:
         cipher = AES.new(key, self.AES_MODE, iv)
         
         try:
-            url_decoded_data: str = parse.unquote(enc_data)
+            url_decoded_data: str = parse.unquote(enc_data) # URL decode
             decode_data: bytes = base64.b64decode(url_decoded_data)
             
             decrypted_data: str = cipher.decrypt(decode_data).decode('euc-kr') # euc-kr 고정
